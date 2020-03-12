3b56d8522077e1d3815262b78094479a2f985577
Analise Burtet <analiseburtet@gmail.com>
2020-03-12 15:32:35 -0300

    adicionado funcao para add evento a collection no firestore

--- src/components/eventos.js
+++ src/components/eventos.js
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
+import firebase from '../base.js'
 
 const Eventos = () => {
+    const [ title,setTitle ] = useState('')
+    function onSubmit(e){
+        e.preventDefault()
+        firebase
+        .firestore()
+        .collection('eventos')
+        .add({
+            title
+        })
+        .then(()=> {
+            setTitle('')
+        })
+    }
+
     return(
-        <div>
-            EVENTOS
-        </div>
+        <form onSubmit={onSubmit}>
+        <input placeholder="Nome do evento" value={title} onChange={e => setTitle(e.currentTarget.value)}/>
+        <button>Criar Evento</button>
+        </form>
     )
 }
 
